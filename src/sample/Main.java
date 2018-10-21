5675f757ef3f4b452942784eec998e837606828a
Rotem <rotem12430@gmail.com>
2018-10-21 12:42:00 +0300

    Started the javafx work

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,5 @@
 package sample;
 
-import Model.DataBaseManager;
-import Model.User;
-
-import java.sql.SQLException;
-
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -16,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
---
+++ src/sample/MainPageController.java
@@ -0,0 +1,9 @@
+package sample;
+
+import javafx.scene.control.Button;
+
+public class MainPageController {
+    public Button LoginButton;
+    public Button SearchButton;
+    public Button SignUpButton;
+}
