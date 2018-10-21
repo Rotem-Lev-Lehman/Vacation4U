89ea01ee90b22ce216930ae90fd99c40433c7167
Paz-Yona <pazyona@gmail.com>
2018-10-21 20:48:18 +0300

    MainPageController

--- src/View/MainPageController.java
+++ src/View/MainPageController.java
@@ -1,5 +1,6 @@
 package View;
 
+import Model.AModel;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -8,20 +9,27 @@ import javafx.scene.text.Text;
 import java.util.Observable;
 
 
-public class MainPageController extends Observable {
+public class MainPageController {
 
 
     public TextField usernameTextField;
     public TextField passwordTextField;
     public Text NotregisteredTextField;
-
+    public AModel model;
     public void LoginPressed(ActionEvent event){
-        //String username = usernameTextField.getText();
-        //String password = passwordTextField.getText();
+        String username = usernameTextField.getText();
+        String password = passwordTextField.getText();
 
-        //search database
-        setChanged();
-        notifyObservers("Login Request");
+        if (username!=null && password!=null)
+        {
+            //search database
+            String[] str = new String[2];
+            str[0] = username;
+            str[1]=password;
+
+            if (model.Read(username).getPassword()
+        }
+
     }
 
     public void UserExists(){
