577a2339f0ec69edf774947ec696dacfa9c36f48
eti reznik <etireznik6@gmail.com>
2018-10-21 19:46:20 +0300

    MainPage fxml

--- src/View/MainPageController.java
+++ src/View/MainPageController.java
@@ -1,9 +1,25 @@
 package View;
 
-import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
 
 public class MainPageController {
-    public Button LoginButton;
-    public Button SearchButton;
-    public Button SignUpButton;
+
+
+    public TextField usernameTextField;
+    public TextField passwordTextField;
+    public Text NotregisteredTextField;
+
+    public void LoginPressed(ActionEvent event){
+        String username = usernameTextField.getText();
+        String password = passwordTextField.getText();
+
+        //search database
+    }
+
+    public void NotRegistedPressed(MouseEvent mouseEvent) {
+    }
 }
