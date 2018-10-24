e9bbaa969f1c808ee1259f1485cda1ede06bde30
Paz-Yona <pazyona@gmail.com>
2018-10-24 18:00:47 +0300

    updateController

--- src/View/UpdateController.java
+++ src/View/UpdateController.java
@@ -17,12 +17,17 @@ public class UpdateController extends AController implements Initializable {
     public TextField FirstNameText;
     public TextField LastNameText;
     public TextField CityText;
+    public TextField UserNameText;
+
     public Button UpdateButton;
     public PasswordField password;
     public Text process_message;
     public PasswordField passwordCnf;
 
-    public UpdateController(TextField firstNameText, TextField lastNameText, TextField cityText, Button updateButton, PasswordField password) {
+    public String oldUsername;
+
+    public UpdateController(TextField UserName, TextField firstNameText, TextField lastNameText, TextField cityText, Button updateButton, PasswordField password) {
+        UserNameText =UserName;
         FirstNameText = firstNameText;
         LastNameText = lastNameText;
         CityText = cityText;
@@ -34,6 +39,8 @@ public class UpdateController extends AController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         if(user != null){
+            oldUsername = UserNameText.getText();
+            UserNameText.setText(user.getUsername());
             FirstNameText.setText(user.getFirstName());
             LastNameText.setText(user.getLastName());
             CityText.setText(user.getCity());
@@ -41,13 +48,23 @@ public class UpdateController extends AController implements Initializable {
         }
     }
     public void Update(){
+        String username = UserNameText.getText();
         String firstname = FirstNameText.getText();
         String lastname = LastNameText.getText();
         String city = CityText.getText();
         String passw = password.getText();
-        if(firstname == null  || lastname == null || city == null || passw == null || password.equals("")){
+        if(username == null  ||firstname == null  || lastname == null || city == null || passw == null || password.equals("") || model.Read(username)==null){
             process_message.setText("Please enter username and password");
             return;
+        }
+        else if(model.Read(username)!=null){
+            process_message.setText("This Username already exist! Please choose another username");
+            return;
+        }
+
+        else  {
+            User user =new User(username,passw,model.Read(oldUsername).getBirthdate(),firstname, lastname, city);
+            model.Update(oldUsername,user);
         }
 
 
