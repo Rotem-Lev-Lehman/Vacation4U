1a1cd4a97ce227fb6abbdce8048c4c1c5cc5ac5c
Rotem <rotem12430@gmail.com>
2018-10-24 18:15:28 +0300

    SearchPage Completed

--- src/View/SearchPageController.java
+++ src/View/SearchPageController.java
@@ -8,8 +8,15 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
 
 import java.net.URL;
 import java.util.List;
@@ -18,25 +25,34 @@ import java.util.ResourceBundle;
 public class SearchPageController extends AController implements Initializable {
 
     public TextField searchTextField;
-    public TableView searchResultTable;
+    public TableView<User> searchResultTable;
+    public TableColumn<User,String> usernameColumn;
+    public TableColumn<User,String> firstNameColumn;
+    public TableColumn<User,String> lastNameColumn;
+    public TableColumn<User,String> cityColumn;
+    public TableColumn<User,String> birthDateColumn;
+    public ImageView magViewBox;
     private ObservableList<User> userObservableList;
 
-    public void searchButtonClicked(ActionEvent actionEvent) {
+    public void searchButtonClicked(MouseEvent mouseEvent) {
         String userName = searchTextField.getText();
 
         if(userName == null || userName.equals("")){
             //say bad input
+            searchResultTable.setItems(null);
         }
         else{
             //Search for the user in the DataBase
             List<User> users = model.ReadSimilar(userName);
             if(users == null || users.size() == 0){
                 //say no results
+                searchResultTable.setItems(null);
             }
             else{
                 //show results
                 userObservableList = FXCollections.observableArrayList(users);
                 //userObservableList.addAll(users);
+
 
                 searchResultTable.setItems(userObservableList);
 
@@ -46,6 +62,12 @@ public class SearchPageController extends AController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        usernameColumn.setCellValueFactory(new PropertyValueFactory<>("username"));
+        firstNameColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        lastNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        cityColumn.setCellValueFactory(new PropertyValueFactory<>("city"));
+        birthDateColumn.setCellValueFactory(new PropertyValueFactory<>("birthdate"));
+        Image magGlass = new Image(this.getClass().getResourceAsStream("/images/mag_glass.png"));
+        magViewBox.setImage(magGlass);
     }
 }
