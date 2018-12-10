bc6a6ffc442a3230eb3b6ef6bd708961a87e52ed
Rotem-Lev-Lehman <rotem12430@gmail.com>
2018-12-10 17:04:21 +0200

    Added functions for communicating with database

--- src/main/java/Model/AModel.java
+++ src/main/java/Model/AModel.java
@@ -10,27 +10,63 @@ public abstract class AModel {
     protected IDataBaseManager dataBaseManager;
 
     //Create user
-    public void Create(User user) throws SQLException {
-        dataBaseManager.Create(user);
+    public void CreateUser(User user) throws SQLException {
+        dataBaseManager.CreateUser(user);
     }
 
     //Read similar users
-    public List<User> ReadSimilar(String username) {
-        return dataBaseManager.ReadSimilar(username);
+    public List<User> ReadSimilarUsers(String username) {
+        return dataBaseManager.ReadSimilarUsers(username);
     }
 
     //Read user
-    public User Read(String username){
-        return dataBaseManager.Read(username);
+    public User ReadUser(String username){
+        return dataBaseManager.ReadUser(username);
     }
 
     //Update user information
-    public void Update(String username, User user) {
-        dataBaseManager.Update(username, user);
+    public void UpdateUser(String username, User user) {
+        dataBaseManager.UpdateUser(username, user);
     }
 
     //Delete user
-    public void Delete(User user) {
-        dataBaseManager.Delete(user);
+    public void DeleteUser(User user) {
+        dataBaseManager.DeleteUser(user);
+    }
+
+    public void CreateVacation(Vacation vacation){
+        dataBaseManager.CreateVacation(vacation);
+    }
+
+    List<Vacation> ReadSimilarVacations(Vacation vacation){
+        return dataBaseManager.ReadSimilarVacations(vacation);
+    }
+
+    void UpdateVacation(Vacation vacation){
+        dataBaseManager.UpdateVacation(vacation);
+    }
+
+    void CreateOrder(Order order){
+        dataBaseManager.CreateOrder(order);
+    }
+
+    List<Order> ReadOrdersForVacation(Vacation vacation){
+        return dataBaseManager.ReadOrdersForVacation(vacation);
+    }
+
+    void UpdateOrder(Order order){
+        dataBaseManager.UpdateOrder(order);
+    }
+
+    void CreatePaymentTransaction(PaymentTransaction paymentTransaction){
+        dataBaseManager.CreatePaymentTransaction(paymentTransaction);
+    }
+
+    void CreateMessage(Message message){
+        dataBaseManager.CreateMessage(message);
+    }
+    
+    List<Message> ReadMessages(String username){
+        return dataBaseManager.ReadMessages(username);
     }
 }
--- src/main/java/Model/DataBaseCommunication/IDataBaseManager.java
+++ src/main/java/Model/DataBaseCommunication/IDataBaseManager.java
@@ -13,5 +13,16 @@ public interface IDataBaseManager {
     void UpdateUser(String username, User user); //Update user information
     void DeleteUser(User user); //Delete User
 
+    void CreateVacation(Vacation vacation); //Create Vacation
+    List<Vacation> ReadSimilarVacations(Vacation vacation); //Read vacations with similar parameters
+    void UpdateVacation(Vacation vacation); //Update a given vacation in the database
 
+    void CreateOrder(Order order); //Create Order
+    List<Order> ReadOrdersForVacation(Vacation vacation);
+    void UpdateOrder(Order order);
+
+    void CreatePaymentTransaction(PaymentTransaction paymentTransaction);
+
+    void CreateMessage(Message message);
+    List<Message> ReadMessages(String username);
 }
